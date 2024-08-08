0211ca70d26964f5fb5ac64eae942d54f0886267
Radim Közl <kozl@svmetal.cz>
2024-08-09 01:28:57 +0200

    feat: Add dataPath.py with Path definition for local use

---
+++ notebooks/dataPath.py
@@ -0,0 +1,8 @@
+import os
+
+DATA_PATH = os.path.join('..','data')
+MODEL_PATH = os.path.join('..','data','models')
+IMAGES_PATH = os.path.join('..', 'data', 'images')
+VIDEOS_PATH = os.path.join('..', 'data', 'videos')
+RESULTS_PATH = os.path.join('..', 'data', 'images', 'results')
+PICKLE_PATH = os.path.join('..', 'data', 'pickle')
